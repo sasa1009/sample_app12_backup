2e4ed2092134d2cb85ccfa3d7f9eddfd2bbf3dc5
sassan <sassan@example.com>
2019-10-07 23:03:16 +0000

    Finish ch13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # => app/views/users/show.html.erb
     # debugger
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   # GET /users/new
@@ -70,15 +71,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(
         :name, :email, :password,
         :password_confirmation)
-    end
-
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     # 正しいユーザーかどうか確認
